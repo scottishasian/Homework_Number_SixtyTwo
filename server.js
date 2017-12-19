84edec0dbd55cc4d7efb8944a0bfa837081eb43f
Kynan <k.song@blueyonder.co.uk>
2017-12-19 21:12:35 +0000

    Can now delete items.

--- server.js
+++ server.js
@@ -47,6 +47,19 @@ MongoClient.connect('mongodb://localhost:27017', function(err, client) {
     });
   });
 
+  app.delete('/api/list', function(req, res) {
+    db.collection('list').remove({}, function(err, result){
+      if(err) {
+        console.log(err);
+        res.status(500);
+        res.send();
+        return;
+      }
+      res.status(204);
+      res.send();
+    })
+  });
+
 
   app.listen(3000, function(){
     console.log("Listening on port 3000");
