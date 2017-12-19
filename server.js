d06ef29d1e995e6ec50956e1fee02e94885a074d
Kynan <k.song@blueyonder.co.uk>
2017-12-19 18:59:08 +0000

    Database created.

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ const express = require('express');
 const parser = require('body-parser');
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+
 app.use(parser.json());
 app.use(express.static('client/build'));
 app.use(parser.urlencoded({extended: true}));
 
-app.listen(3000, function(){
-  console.log("Listening on port 3000");
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
+
+  if(err) {
+    console.log(err);
+    return;
+  }
+
+  const db = client.db("alcohol");
+  console.log("I done connected to db");
+
+
+  app.listen(3000, function(){
+    console.log("Listening on port 3000");
+  });
+
+
 });
